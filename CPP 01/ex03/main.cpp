6dfb6af51f30c1466518edcdec03c4c110f09505
khuynh <khuynh@student.42.fr>
2023-06-19 16:27:42 +0200

    lol

--- CPP 01/ex03/main.cpp
+++ CPP 01/ex03/main.cpp
@@ -6,7 +6,32 @@
 /*   By: khuynh <khuynh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 23:35:15 by khuynh            #+#    #+#             */
-/*   Updated: 2023/06/09 23:35:16 by khuynh           ###   ########.fr       */
+/*   Updated: 2023/06/19 16:20:45 by khuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int	main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
