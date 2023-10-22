ca60116e0bf4691ce4cf0c54feb7cddc237f0484
kellycious <kellyhuynh@outlook.fr>
2023-10-22 16:39:12 +0200

    ex00

--- CPP 06/ex00/ScalarConverter.cpp
+++ CPP 06/ex00/ScalarConverter.cpp
@@ -6,8 +6,131 @@
 /*   By: khuynh <khuynh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 18:07:23 by khuynh            #+#    #+#             */
-/*   Updated: 2023/10/19 18:07:31 by khuynh           ###   ########.fr       */
+/*   Updated: 2023/10/22 16:39:05 by khuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+ScalarConverter::ScalarConverter(ScalarConverter const &src) { *this = src; }
+ScalarConverter::~ScalarConverter() {}
+
+ScalarConverter &ScalarConverter::operator=(ScalarConverter const &cpy)
+{
+	(void)cpy;
+	return *this;
+}
+
+static void Cchar(std::string literal)
+{
+	std::cout << "char: '" << literal[0] << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(literal[0]) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(literal[0]) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(literal[0]) << std::endl;
+}
+
+static void Cint(std::string literal)
+{
+	int nb = atoi(literal.c_str());
+	std::cout << "char: ";
+	if (nb < 32 || nb == 126)
+		std::cout << "Non displayable" << std::endl;
+	else if (nb < 0 || nb > 127)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << "'" << literal[0] << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(nb) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(2) << nb << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(2) << static_cast<double>(nb) << std::endl;
+}
+
+static void Cfloat(std::string literal)
+{
+	float nb = atof(literal.c_str());
+	std::cout << "char: ";
+	if (nb < 32 || nb == 126)
+		std::cout << "Non displayable" << std::endl;
+	else if (nb < 0 || nb > 127)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << "'" << literal[0] << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(nb) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(2) << nb << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(2) << static_cast<double>(nb) << std::endl;
+}
+
+static void Dconvert(std::string literal)
+{
+	double nb = atof(literal.c_str());
+	std::cout << "char: ";
+	if (nb < 32 || nb == 126)
+		std::cout << "Non displayable" << std::endl;
+	else if (nb < 0 || nb > 127)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << "'" << literal[0] << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(nb) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(2) << static_cast<float>(nb) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(2) << nb << std::endl;
+}
+
+static void MIconvert()
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: " << INT_MIN << std::endl;
+	std::cout << "float: " << __FLT_MIN__ << std::endl;
+	std::cout << "double: " << __DBL_MIN__ << std::endl;
+}
+
+static void MXconvert()
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: " << INT_MAX << std::endl;
+	std::cout << "float: " << __FLT_MAX__ << std::endl;
+	std::cout << "double: " << __DBL_MAX__ << std::endl;
+}
+
+static void Econvert()
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: " << 0 << std::endl;
+	std::cout << "float: " << 0 << std::endl;
+	std::cout << "double: " << 0 << std::endl;
+}
+
+void	ScalarConverter::convert(std::string const &literal)
+{
+	if (literal.length() == 1 && !isdigit(literal[0]))
+		Cchar(literal);
+	else if (literal == "nan" || literal == "nanf")
+		Econvert();
+	else if (literal == "-inff" ||literal == "-inf")
+		MIconvert();
+	else if (literal == "+inff" || literal == "+inf" || literal == "inf" || literal == "inff")
+		MXconvert();
+	else if (literal[literal.length() - 1] == 'f')
+	{
+		long unsigned int i = 0;
+		while ((i < literal.length() - 1 && std::isdigit(literal[i])) || literal[i] == '.')
+			i++;
+		if (i == literal.length() - 1)
+			Cfloat(literal);
+		else
+			Econvert();
+	}
+	else if (literal.find('.') != std::string::npos)
+	{
+		long unsigned int i = 0;
+		while (std::isdigit(literal[i]) || literal[i] == '.')
+			i++;
+		if (i == literal.length())
+			Dconvert(literal);
+		else
+			Econvert();
+	}
+	else if (std::isdigit(literal[0]))
+		Cint(literal);
+	else
+		Econvert();
+}
--- CPP 06/ex00/main.cpp
+++ CPP 06/ex00/main.cpp
@@ -1,33 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khuynh <khuynh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/19 18:07:14 by khuynh            #+#    #+#             */
-/*   Updated: 2023/10/22 16:32:34 by khuynh           ###   ########.fr       */
+/*   Created: 2023/10/22 16:30:24 by khuynh            #+#    #+#             */
+/*   Updated: 2023/10/22 16:35:09 by khuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCALARCONVERTER_HPP
-# define SCALARCONVERTER_HPP
+#include "ScalarConverter.hpp"
 
-# include <iostream>
-# include <string>
-# include <iomanip>
-# include <limits.h>
-
-class ScalarConverter
+int main ()
 {
-	public:
-		ScalarConverter();
-		ScalarConverter(ScalarConverter const &src);
-		~ScalarConverter();
-
-		ScalarConverter &operator=(ScalarConverter const &cpy);
-
-		void convert(std::string const &literal);
-};
-
-#endif
+	std::string literal;
+	std::cout << "Enter a literal: ";
+	std::cin >> literal;
+	ScalarConverter sc;
+	sc.convert(literal);
+}
